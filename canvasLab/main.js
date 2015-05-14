4520a385ce6da6b555e5281b4c156419c72faadf
superchangme <superchangme@126.com>
2015-05-14 18:00:01 +0800

    update require js module

--- canvasLab/main.js
+++ canvasLab/main.js
@@ -4,19 +4,12 @@ require.config({
         'jquery-eraser': ['jquery']
     },
     paths: {
-    "jquery": "jquery",
+    "jquery": "jquery-2.1.4",
     "jquery-eraser":"fingerEraser",
     "underscore": "underscore.min",
     "backbone": "backbone.min",
-     "tomLib":"tomLib"
+     "tomLib":"tomLib",
+     "zepto":"zepto.min"
     }
 });
-require(["jquery","jquery-eraser"],function($,eraser){
-    var bg = new Image();
-    bg.src = "../source/img/b.jpg";
-    bg.onload=function(){
-       $("#eraserArea").eraser({bgImage:bg,percent:0.5,density:4,container:$("#eraserArea")[0],success:function(){
-           alert("成功擦除50%")
-       }});
-    }
-})
+
--- vendor/main.js
+++ vendor/main.js
@@ -1,12 +1,25 @@
 require.config({
+    /*模块依赖配置*/
     shim: {
         'jquery-eraser': ['jquery']
     },
-paths: {
-"jquery": "jquery",
-"jquery-eraser":"fingerEraser",
-"underscore": "underscore.min",
-"backbone": "backbone.min",
- "tomLib":"tomLib"
-}
+    /*模块路径配置*/
+    paths: {
+    "jquery": "jquery",
+    "jquery.eraser":"jquery.eraser",
+    "underscore": "underscore.min",
+    "backbone": "backbone.min",
+     "tomLib":"tomLib",
+     "jquery-private":"jquery-private",
+    "tomRequire":"tomRequire"
+    }  ,
+    /*模块规则配置*/
+    map:{
+        '*': {
+            'jquery': 'jquery-private'
+        },
+        "jquery-private":{
+            "jquery":"jquery"
+        }
+    }
 });
