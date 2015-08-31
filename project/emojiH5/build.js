deebadd14ea73a91af1f1da3298b4250272195a2
superchangme <superchangme@126.com>
2015-08-31 13:03:20 +0800

    require r.js

---
+++ project/emojiH5/build.js
@@ -0,0 +1,25 @@
+({
+    baseUrl: '../../vendor',
+    dir: "./build",
+    modules: [
+        {
+            name: 'app'
+        }
+    ],
+    include:"app.js",
+    fileExclusionRegExp: /^(r|build)\.js$/,
+    paths: {
+        "jquery": "jquery-2.1.4",
+        "jquery.eraser":"jquery.eraser",
+        "tomLib":"tom.Lib",
+        "jquery-private":"jquery-private",
+        "jcanvas":"jcanvas",
+        "hammer":"Hammer/hammer.min",
+        "hammer.fake":"Hammer/hammer.fakemultitouch",
+        "hammer.showtouch":"Hammer/hammer.showtouches",
+        "tomPlugin":"plugins/tom-jqplugins",
+        'Caman':"caman.full",
+        'slider':"rangeslider.js-1.3.3/rangeslider.min",
+        "iscroll-lite":"iscroll-lite"
+    }
+})
